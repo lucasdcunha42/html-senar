c70ef41c20a26f7f9f26b9b92b285d01bbc060bd
lucas.cunha <lucas.cunha@senar-rs.com.br>
2023-07-10 10:40:03 -0300

    Ordenação legislação

--- app/Http/Controllers/Frontend/InstitucionalController.php
+++ app/Http/Controllers/Frontend/InstitucionalController.php
@@ -38,12 +38,12 @@ class InstitucionalController extends Controller
         $legislacaoCategorias = \App\LegislacoesCategoria::with([
             'legislacoes' => function ($query) {
                 $query->where('status', 1);
-                $query->orderBy('created_at', 'desc');
-                $query->orderBy('id', 'desc');
+                $query->orderBy('ordem', 'desc');
+                $query->orderBy('updated_at', 'desc');
             }
         ])
             ->where('status', 1)
-            ->orderBy('created_at', 'desc')
+            ->orderBy('ordem', 'desc')
             ->orderBy('id', 'desc')
             ->get();
 
