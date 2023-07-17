3179694ce5d9bde5b4e9c2293af09ed24b76e43a
lucas.cunha <lucas.cunha@senar-rs.com.br>
2023-07-17 16:54:41 -0300

    relacionamento sindicato

--- app/Agenda.php
+++ app/Agenda.php
@@ -13,4 +13,9 @@ class Agenda extends CustomModel
         return $this->belongsTo(Curso::class, 'cod_curso', 'cod_curso');
     }
 
+    public function sindicato()
+    {
+        return $this->belongsTo(Sindicato::class, 'nome_sindicato', 'nome');
+    }
+
 }
