1ef1bd3fa523dc608a9e0f4b183c36c7bf3378d0
lucas.cunha <lucas.cunha@senar-rs.com.br>
2023-07-17 09:05:41 -0300

    functions on models

--- app/Noticia.php
+++ app/Noticia.php
@@ -22,4 +22,46 @@ class Noticia extends CustomModel
         }
         return '';
     }
+
+    public function getArrayFilesPath()
+    {
+        $arquivos = json_decode($this->arquivo);
+
+        if(!is_array($arquivos)) {
+            return null;
+        }
+
+        if(!count($arquivos)) {
+            return null;
+        }
+
+        $arr = [];
+
+        foreach($arquivos as $arquivo) {
+            $arr[] = $arquivo->download_link;
+        }
+        return $arr;
+    }
+
+    public function getArrayFiles()
+    {
+
+        $arquivos = json_decode($this->arquivo);
+
+        if(!is_array($arquivos)) {
+            return null;
+        }
+
+        if(!count($arquivos)) {
+            return null;
+        }
+
+        $arr = [];
+
+        foreach($arquivos as $arquivo) {
+            $arr[] = $arquivo;
+        }
+        return $arr;
+    }
+
 }
