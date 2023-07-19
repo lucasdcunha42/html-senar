8805c66b4cd6e13f7e2f2bec56f4624539cf53b3
lucas.cunha <lucas.cunha@senar-rs.com.br>
2023-07-19 14:43:19 -0300

    altera variaveis card agenda

--- resources/views/components/c-thumb-4.blade.php
+++ resources/views/components/c-thumb-4.blade.php
@@ -1,22 +1,23 @@
 <div class="c-thumb-4">
     <div class="c-thumb-4__date">
-        <span>{{ $evento->curso->getAttrDateFromFormat('data_inicio', 'Y-m-d', 'd.m.Y') }}</span>
+        <span>{{ $evento->getAttrDateFromFormat('data_inicio', 'Y-m-d', 'd.m.Y') }}</span>
     </div>
     <div class="c-thummb-4__title">
         <div class="d-table">
             <div class="d-table-cell v-center h-center">
-                <h4>{{ $evento->curso->nome_curso }}</h4>
+
+                <h4>{{ $evento->nome_curso }}</h4>
             </div>
         </div>
     </div>
     <div class="c-thummb-4__city">
-        <p>{{ $evento->curso->cidade }}</p>
+        <p>{{ $evento->cidade }}</p>
     </div>
     <div class="c-thumb-4__link">
-        <a href="{{ route('page.cursos.single', $evento->curso->slug) }}">Ler mais</a>
+        <a href="{{ route('page.cursos.single', $evento->slug) }}">Ler mais</a>
     </div>
     {{-- --}}
-    @if($evento->curso->desc_fase_evento == 'Cancelado')
+    @if($evento->desc_fase_evento == 'Cancelado')
         <div class="curso-cancelado">
             Cancelado
         </div>
