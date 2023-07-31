752892aaa6dc94bb0109c0084511768d4a166409
lucasdcunha42 <lucasdcunha42@gmail.com>
2023-07-31 17:02:14 -0300

    pdf modal

--- resources/views/frontend/pages/institucional/legislacao.blade.php
+++ resources/views/frontend/pages/institucional/legislacao.blade.php
@@ -44,11 +44,11 @@
                                         <div class="accord-desc">
                                             @foreach($categoria->legislacoes as $legislacao)
                                                 <div class="link-legislacao">
-    
+
                                                     @php $files = $legislacao->getArrayFiles() @endphp
                                                     @if(isset($files[0]) && !empty($files[0]->download_link))
                                                         <div>
-                                                            <a target="_blank" href="{{ route('download') . '?file=' . $files[0]->download_link }}">
+                                                            <a target="_blank" class="legislacao-link" data-pdf="{{ 'storage/' . $files[0]->download_link }}" >
                                                                 {!! $legislacao->titulo !!}
                                                             </a>
                                                         </div>
@@ -67,6 +67,21 @@
                                     </div>
                                 @endforeach
                             </div>
+                            <div class="modal fade" id="pdfModal" tabindex="-1" role="dialog" aria-labelledby="pdfModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="pdfModalLabel">Visualizar PDF</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <iframe id="pdfIframe" width="100%" height="700px"></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -75,4 +90,23 @@
     @endif
 
 
+@endsection
+
+@section('js')
+    <script>
+        // Evento para abrir o modal e carregar o PDF ao clicar no link da legislação
+        $('.legislacao-link').click(function(e) {
+            pdfLink = $(this).data('pdf');
+            console.log(pdfLink);
+
+            $('#pdfIframe').attr('src', pdfLink);
+            $('#pdfModal').modal('show');
+        });
+
+        // Evento para limpar a URL do PDF quando o modal for fechado
+        $('#pdfModal').on('hidden.bs.modal', function () {
+            $('#pdfIframe').attr('src', '');
+            pdfLink = '';
+        });
+    </script>
 @endsection
