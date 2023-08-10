da62ecdbc7b0508d793a60fd492391a59f6f7c02
lucasdcunha42 <lucasdcunha42@gmail.com>
2023-08-10 14:49:37 -0300

    mutiple files  o noticias

--- resources/views/frontend/news/single.blade.php
+++ resources/views/frontend/news/single.blade.php
@@ -52,6 +52,71 @@
             </div>
         </div>
 
+        <div class="accordion-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="accordion-container">
+                            <h2> Arquivos </h2>
+                            <div class="link-legislacao">
+
+                                @php
+                                    $files = $noticia->getArrayFiles();
+                                @endphp
+
+                                @if(count($files) > 0)
+                                    <ul class="file-list">
+                                        @foreach($files as $file)
+                                            @if(!empty($file->download_link))
+                                                <li>
+                                                    <a target="_blank" class="legislacao-link" data-pdf="{{ asset('storage/' . $files[0]->download_link) }}">
+                                                        {!! $file->original_name !!}
+                                                    </a>
+                                                </li>
+                                            @endif
+                                        @endforeach
+                                    </ul>
+                                @else
+                                    <div class="link-legislacao">
+                                        <h4>Nenhum arquivo disponível.</h4>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="modal fade custom-modal" id="pdfModal" tabindex="-1" role="dialog" aria-labelledby="pdfModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg custom-modal-dialog" role="document">
+                                    <div class="modal-content custom-modal-content">
+                                        <div class="modal-header">
+                                            <div class="row align-items-center">
+                                                <!-- Botão de fechar (coluna 1) -->
+                                                <div class="col-4 text-right">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar" style="padding-right: 20px;">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <!-- Logo (coluna 2) -->
+                                                <div class="col-4 text-center">
+                                                    <img src="{{ asset('storage/' . setting('site.logo')) }}" alt="Logo" class="img-fluid" style="max-height: 80px;">
+                                                </div>
+                                                <!-- Título (coluna 3) -->
+                                                <div class="col-4 text-center">
+                                                    <h5 class="modal-title" id="pdfModalLabel">{!! $file->original_name !!}</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="modal-body">
+                                            <iframe id="pdfIframe" width="100%" height="700px"></iframe>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         @if(!empty($galeria))
             <br><br><br>
             <div class="galeria-de-images bg-grey">
@@ -123,4 +188,25 @@
         </div>
     </div>
 
+@endsection
+
+@section('js')
+    <script>
+
+        // Evento para abrir o modal e carregar o PDF ao clicar no link da legislação
+        $('.legislacao-link').click(function(e) {
+            pdfLink = $(this).data('pdf');
+            console.log(pdfLink);
+
+            $('#pdfIframe').attr('src', pdfLink);
+            $('#pdfModal').modal('show');
+        });
+
+        // Evento para limpar a URL do PDF quando o modal for fechado
+        $('#pdfModal').on('hidden.bs.modal', function () {
+            $('#pdfIframe').attr('src', '');
+            pdfLink = '';
+        });
+
+    </script>
 @endsection
