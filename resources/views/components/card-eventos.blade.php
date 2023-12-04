aebf0584ed91fb5db8253b7d53949e0207db7a77
lucasdcunha42 <lucasdcunha42@gmail.com>
2023-12-04 16:11:30 -0300

    fix scss comp

--- resources/views/components/card-eventos.blade.php
+++ resources/views/components/card-eventos.blade.php
@@ -1,19 +1,29 @@
-<div class="card-evento">
-    <div class="card-header-evento">
-        <img src="https://static.wixstatic.com/media/1c71a7_37971b8a97b940f29396f74fdfed4cf1~mv2.png/v1/fill/w_1000,h_1000,al_c,q_90,usm_0.66_1.00_0.01/1c71a7_37971b8a97b940f29396f74fdfed4cf1~mv2.png" alt="Image Title" />
+<section class="light">
+	<div class="container py-2">
+		<article class="postcard light green">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="https://picsum.photos/500/501" alt="Image Title" />
+			</a>
+			<div class="postcard__text t-dark">
+				<h1 class="postcard__title green"><a href="#">Palestra: Terá Pão de queijo?</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i>Sexta, Dez 01th 2023
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Na iminência da tão esperada sexta-feira, surge uma possibilidade que desperta sorrisos e antecipa o aroma irresistível de uma iguaria brasileira: o pão de queijo. Entre os colegas, a expectativa cresce à medida que a ideia de Luiz trazer essa delícia para compartilhar ganha força.
+                    A sugestão de Luiz, um apreciador declarado de bons momentos e sabores, encontra eco na vontade coletiva de celebrar o fim de semana que se aproxima. O pão de queijo, com sua crocância por fora e maciez por dentro, torna-se não apenas um quitute, mas um símbolo de confraternização e alegria.
+                </div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-clock mr-2"></i>Inscrever-se</li>
+					<li class="tag__item play green">
+						<a href="#"><i class="fas fa-play mr-2"></i>Ver Mais...</a>
+					</li>
+				</ul>
+			</div>
+		</article>
     </div>
-    <div class="card-body-evento">
-        <h1 class="titulo-evento"> Titulo </h1>
-
-        <p class="texto-evento">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur autem, saepe impedit veniam id necessitatibus labore. Rerum, ipsa ducimus tempora minima cum similique, deserunt nisi cumque facilis libero incidunt deleniti.
-        </p>
-
-        <div class="botoes-evento">
-            <span class="mais-informacoes">Mais Informaçoes</span>
-            <button class="inscrevase"> Inscreva-se </button>
-        </div>
-    </div>
-  </div>
+</section>
 
 
