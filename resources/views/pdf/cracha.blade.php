b40ce6dbd88a0c6224d8aca0ffd95f26057c4e3c
lucasdcunha42 <lucasdcunha42@gmail.com>
2024-02-01 16:59:39 -0300

    chracha, bug image

---
+++ resources/views/pdf/cracha.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+    <div>
+        <img src="{{ public_path('assets/images/cracha.png') }}" alt="image">
+        <h1>{{ $data['evento'] }}</h1>
+    </div>
+</body>
+</html>
